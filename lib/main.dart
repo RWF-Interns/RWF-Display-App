cc40c5f35725d484d6bd0b9e177cfe919ac1f502
SaraKhatoon <sarakhatoon4699@gmail.com>
2020-01-22 20:23:45 +0530

    flutter app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:RWF/screens/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Rail Wheel Factory',
+      home: HomePage(),
+    );
+  }
+}
